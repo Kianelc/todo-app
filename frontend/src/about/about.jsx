42fbf89d8955f7934dd2232bf171b15236fc5ef6
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-08-08 13:19:42 -0300

    página sobre

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default props => (
-    <div>
-        <h1>Sobre</h1>
-    </div>
-)
+  <div>
+    <PageHeader name="Sobre" small="Nós"></PageHeader>
+    <h2>Nossa História</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi expedita
+      iste molestiae repellendus tempore quam adipisci sunt? Possimus itaque cum
+      hic, in repudiandae asperiores quas soluta nobis consectetur, beatae
+      labore?
+    </p>
+    <h2>Missão e Visão</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+      voluptates non ipsam eveniet ea ullam cupiditate numquam perspiciatis
+      officiis. Veritatis, nam nulla? Facere eius vel necessitatibus.
+      Exercitationem saepe nostrum autem!
+    </p>
+    <h2>Imprensa</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nemo
+      sapiente sit excepturi. Sint facilis quaerat doloremque distinctio beatae
+      provident, nam nemo, accusantium ea accusamus est libero inventore ab!
+      Animi?
+    </p>
+  </div>
+);
